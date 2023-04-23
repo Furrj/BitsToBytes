933b6f566c776c5a3456f4ecb4dafe0d027fea61
Jackson Furr <jackson.a.furr@gmail.com>
2023-04-23 02:01:01 -0400

    added redux to allow for tracking RAM state

--- src/components/RAM/Registers.tsx
+++ src/components/RAM/Registers.tsx
@@ -1,11 +1,21 @@
 import React from "react";
 import styles from "./Registers.module.css";
 
+//REDUX
+import type { RootState } from "../../data/store";
+import { useSelector, useDispatch } from "react-redux";
+import { increment, decrement, setAddress } from "../../data/ramSlice";
+
 //COMPS
 import AddressColumn from "./Columns/AddressColumn";
 import ValueColumn from "./Columns/ValueColumn";
 
 const Registers: React.FC = () => {
+  const currentAddress: number = useSelector(
+    (state: RootState) => state.ram.currentAddress
+  );
+  const dispatch = useDispatch();
+
   return (
     <div className={styles.register}>
       <AddressColumn />
