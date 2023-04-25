bbf9352f2b19f788698bb0a3248033acb9ad25f8
Jackson Furr <jackson.a.furr@gmail.com>
2023-04-25 17:49:05 -0400

    styles accumulator component

--- src/components/Registers/Accumulator.tsx
+++ src/components/Registers/Accumulator.tsx
@@ -5,7 +5,14 @@ import Draggable from "react-draggable";
 const Accumulator: React.FC = () => {
   return (
     <Draggable>
-      <div className={styles.accumulator}>Accumulator</div>
+      <div className={styles.accumulator}>
+        <div className={styles.title}>Accumulator</div>
+        <div className={styles.valueCont}>
+          <div className={styles.valueBox}>
+            <h1 className={styles.value}>8</h1>
+          </div>
+        </div>
+      </div>
     </Draggable>
   );
 };
