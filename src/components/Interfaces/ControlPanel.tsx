51c526434d6b3cf874556fd602f5b8294c6a8a0c
Jackson Furr <jackson.a.furr@gmail.com>
2023-04-25 18:19:36 -0400

    addressBox layout

--- src/components/Interfaces/ControlPanel.tsx
+++ src/components/Interfaces/ControlPanel.tsx
@@ -7,7 +7,14 @@ const ControlPanel: React.FC = () => {
     <Draggable>
       <div className={styles.controlPanel}>
         <div className={styles.title}>Control Panel</div>
-        <div className={styles.panelCont}></div>
+        <div className={styles.panelCont}>
+          <div className={styles.addressBox}>
+            <div className="boxTitle">Address</div>
+            <input type="number" name="address" id="address" />
+            <button>Set</button>
+            <div className={styles.currentValue}>9</div>
+          </div>
+        </div>
       </div>
     </Draggable>
   );
