3e6a7f58fdb147901e1c134f58022ad3525f0df2
Jackson Furr <jackson.a.furr@gmail.com>
2023-04-25 18:33:20 -0400

    basic control panel UI layout

--- src/components/Interfaces/ControlPanel.tsx
+++ src/components/Interfaces/ControlPanel.tsx
@@ -9,16 +9,23 @@ const ControlPanel: React.FC = () => {
         <div className={styles.title}>Control Panel</div>
         <div className={styles.panelCont}>
           <div className={styles.addressBox}>
-            <div className="boxTitle">Address</div>
+            <div className={styles.boxTitle}>Address</div>
             <input type="number" name="address" id="address" />
             <button>Set</button>
             <div className={styles.currentValue}>9</div>
           </div>
           <div className={styles.valueBox}>
-            <div className="boxTitle">Value</div>
+            <div className={styles.boxTitle}>Value</div>
             <input type="number" name="address" id="address" />
             <button>Set</button>
             <div className={styles.currentValue}>9</div>
+          </div>
+          <div className={styles.accumulatorBox}>
+            <div className={styles.boxTitle} style={{ marginLeft: "2px" }}>
+              Accumulator
+            </div>
+            <button>Load</button>
+            <button>Store</button>
           </div>
         </div>
       </div>
