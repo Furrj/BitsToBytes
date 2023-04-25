e2b8c7b066a2a70e2f64ae255ce534210ceb7fe8
Jackson Furr <jackson.a.furr@gmail.com>
2023-04-25 18:53:06 -0400

    input func, allows user to change RAM address and value

--- src/components/Interfaces/ControlPanel.tsx
+++ src/components/Interfaces/ControlPanel.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./ControlPanel.module.css";
 import Draggable from "react-draggable";
 
@@ -7,12 +7,29 @@ import { useSelector, useDispatch } from "react-redux";
 import * as ramReducer from "../../data/ramSlice";
 import { RootState } from "../../data/store";
 
+//STATE
+interface IInput {
+  address: number;
+  value: number;
+}
+
+const initInput: IInput = {
+  address: 0,
+  value: 0,
+};
+
 const ControlPanel: React.FC = () => {
+  const [input, setInput] = useState<IInput>(initInput);
+
   const values: number[] = useSelector((state: RootState) => state.ram.data);
   const address: number = useSelector(
     (state: RootState) => state.ram.currentAddress
   );
   const dispatch = useDispatch();
+
+  function inputHandler(e: React.ChangeEvent<HTMLInputElement>): void {
+    setInput({ ...input, [e.target.name]: e.target.value });
+  }
 
   return (
     <Draggable>
@@ -21,16 +38,32 @@ const ControlPanel: React.FC = () => {
         <div className={styles.panelCont}>
           <div className={styles.addressBox}>
             <div className={styles.boxTitle}>Address</div>
-            <input type="number" name="address" id="address" />
-            <button onClick={() => dispatch(ramReducer.setAddress(2))}>
+            <input
+              type="number"
+              name="address"
+              id="address"
+              onChange={inputHandler}
+            />
+            <button
+              onClick={() => dispatch(ramReducer.setAddress(input.address))}
+            >
               Set
             </button>
             <div className={styles.currentValue}>{address}</div>
           </div>
           <div className={styles.valueBox}>
             <div className={styles.boxTitle}>Value</div>
-            <input type="number" name="address" id="address" />
-            <button onClick={() => dispatch(ramReducer.setValue([0, 20]))}>
+            <input
+              type="number"
+              name="value"
+              id="value"
+              onChange={inputHandler}
+            />
+            <button
+              onClick={() =>
+                dispatch(ramReducer.setValue([input.address, input.value]))
+              }
+            >
               Set
             </button>
             <div className={styles.currentValue}>{values[address]}</div>
