d22efb9a813da9ed8a55010b95b362a3688f3767
Jackson Furr <jackson.a.furr@gmail.com>
2023-04-27 16:50:14 -0400

    test

--- src/App.tsx
+++ src/App.tsx
@@ -33,6 +33,7 @@ const App: React.FC = () => {
       })}
       <ControlPanel />
       <InstructionInput />
+      test
       <ComponentToolbar
         components={components}
         updateComponents={updateActiveComponents}
